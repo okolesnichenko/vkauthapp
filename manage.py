ae79082fddf8695b20033655fd18e73f87c4f25c
worldintro <worldintro>
2019-05-04 16:13:26 +0300

    vkapp

--- manage.py
+++ manage.py
@@ -12,6 +12,10 @@ migrate = Migrate(app, db)
 def make_shell_context():
     return dict(app=app, db=db)
 
+def deploy():
+    from flask_migrate import upgrade
+    upgrade()
+
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
