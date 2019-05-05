a5c319528120c4473c8f3e718813c8fa820b9b3e
worldintro <worldintro>
2019-05-05 11:19:47 +0300

    vkauthapp

--- config.py
+++ config.py
@@ -6,6 +6,12 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     VK_API_VERSION = 5.95
+    OAUTH_CREDENTIALS = {
+        'vk': {
+            'key': os.environ.get('VK_KEY'),
+            'secret': os.environ.get('VK_SECRET')
+        }
+    }
     @staticmethod
     def init_app(app):
         pass
@@ -13,22 +19,10 @@ class Config:
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
-    OAUTH_CREDENTIALS = {
-        'vk':{
-            'key':os.environ.get('VK_KEY'),
-            'secret':os.environ.get('VK_SECRET')
-        }
-    }
     DEBUG = True
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    OAUTH_CREDENTIALS = {
-        'vk': {
-            'key': os.environ.get('VK_KEY'),
-            'secret': os.environ.get('VK_SECRET')
-        }
-    }
     DEBUG = True
 
 config = {
